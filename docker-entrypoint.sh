cc95b6fbeb849f74c518edb8bb2e01b7fa94db94
Quphoria <s.simpson41.ss@googlemail.com>
2025-09-03 20:48:24 +0100

    Update permissions on start

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+chown root:lp /etc/cups/cupsd.conf
+chown root:lp /etc/cups/printers.conf
+
 printf "${ADMIN_PASSWORD:-cupsadmin}\n${ADMIN_PASSWORD:-cupsadmin}" | passwd admin &> /dev/null
 
 echo "Starting cupsd..."
