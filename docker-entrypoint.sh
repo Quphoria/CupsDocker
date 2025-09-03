1cd6dc2abaf5e6c645f372fd317610dff21b6c2a
Quphoria <s.simpson41.ss@googlemail.com>
2025-09-03 21:22:14 +0100

    Fix default files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Make ppd and ssl directories if needed
+# cups can't make server credentials if ssl directory is missing
+mkdir -p /config/ppd /config/ssl
+
 if [ ! -f /config/cupsd.conf ]; then
+    # copy default cups files
+    cp -R /etc/cups/* /config
     # set max jobs
-    cp /etc/cups/cupsd.conf.default /config/cupsd.conf
+    cp /config/cupsd.conf.default /config/cupsd.conf
     sed -i "s/ErrorPolicy stop-printer/ErrorPolicy abort-job\nMaxJobs 10/" /config/cupsd.conf
     # allow remote administration
     sed -i 's/Listen localhost:631/Listen *:631\nServerAlias */' /config/cupsd.conf
